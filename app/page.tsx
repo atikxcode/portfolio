9739ee362970f6ef9927a1231165e52b38c11852
atikxcode <liadbd20@gmail.com>
2024-07-26 18:16:18 +0600

    testimonial section working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Clients from '@/components/Clients'
 import Grid from '@/components/Grid'
 import Hero from '@/components/Hero'
+import RecentProjects from '@/components/RecentProjects'
 import { FloatingNav } from '@/components/ui/FloatingNav'
+import { navItems } from '@/data'
 import Image from 'next/image'
 import { FaHome } from 'react-icons/fa'
 
@@ -8,11 +11,11 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="w-full">
-        <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome></FaHome> }]}
-        ></FloatingNav>
+        <FloatingNav navItems={navItems}></FloatingNav>
         <Hero></Hero>
         <Grid></Grid>
+        <RecentProjects></RecentProjects>
+        <Clients></Clients>
       </div>
     </main>
   )
